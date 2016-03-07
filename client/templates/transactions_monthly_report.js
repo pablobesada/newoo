a078f47f33affd2346a95ea1a8fe6ba4b758dca8
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2016-03-07 13:48:29 -0300

    commit

--- client/templates/transactions_monthly_report.js
+++ client/templates/transactions_monthly_report.js
@@ -10,6 +10,7 @@ Template.transactions_monthly_report.onCreated(function () {
     });
     var apartments_subscription = instance.subscribe('Apartments');
     instance.mode = new ReactiveVar('total');
+    instance.usermode = new ReactiveVar('total');
     instance.records = new ReactiveVar({});
     instance.autorun( function () {
         var start = instance.period.get().start;
@@ -32,24 +33,34 @@ Template.transactions_monthly_report.onCreated(function () {
             _(recs.fetch()).each(function (rec) {
                 if (!data[rec.month]) data[rec.month] = {};
                 if (!data[rec.month][rec.apartment]) data[rec.month][rec.apartment] = {}
-                if (!data[rec.month][rec.apartment][rec.user]) data[rec.month][rec.apartment][rec.user] = 0.0
-                if (!data[rec.month][rec.apartment]['total']) data[rec.month][rec.apartment]['total'] = 0.0
-                if (!data[rec.month][rec.apartment]['administration']) data[rec.month][rec.apartment]['administration'] = 0.0
-                data[rec.month][rec.apartment][rec.user] += rec.amount;
-                data[rec.month][rec.apartment]['total'] += rec.amount;
-                data[rec.month][rec.apartment]['administration'] += administration_percents[rec.apartment] * rec.amount / 100.0;
-
+                if (!data[rec.month][rec.apartment]['total']) data[rec.month][rec.apartment]['total'] = {}
+                if (!data[rec.month][rec.apartment]['total']['total']) data[rec.month][rec.apartment]['total']['total'] = 0.00
+                if (!data[rec.month][rec.apartment]['total'][rec.user]) data[rec.month][rec.apartment]['total'][rec.user] = 0.00
+                if (!data[rec.month][rec.apartment]['administration']) data[rec.month][rec.apartment]['administration'] = {}
+                if (!data[rec.month][rec.apartment]['administration']['total']) data[rec.month][rec.apartment]['administration']['total'] = 0.0
+                if (!data[rec.month][rec.apartment]['administration'][rec.user]) data[rec.month][rec.apartment]['administration'][rec.user] = 0.0
+                data[rec.month][rec.apartment]['total']['total'] += rec.amount;
+                data[rec.month][rec.apartment]['total'][rec.user] += rec.amount;
+                data[rec.month][rec.apartment]['administration']['total'] += administration_percents[rec.apartment] * rec.amount / 100.0;
+                data[rec.month][rec.apartment]['administration'][rec.user] += administration_percents[rec.apartment] * rec.amount / 100.0;
+                /*
                 if (!data[rec.month][rec.user]) data[rec.month][rec.user] = {}
                 if (!data[rec.month][rec.user]['total']) data[rec.month][rec.user]['total'] = 0.00
                 if (!data[rec.month][rec.user]['administration']) data[rec.month][rec.user]['administration'] = 0.00
                 data[rec.month][rec.user]['total'] += rec.amount
                 data[rec.month][rec.user]['administration'] += administration_percents[rec.apartment] * rec.amount / 100.0;
-
+                */
                 if (!data[rec.month]['Total']) data[rec.month]['Total'] = {}
-                if (!data[rec.month]['Total']['total']) data[rec.month]['Total']['administration'] = 0.00;
-                if (!data[rec.month]['Total']['administration']) data[rec.month]['Total']['total'] = 0.00;
-                data[rec.month]['Total']['total'] += rec.amount
-                data[rec.month]['Total']['administration'] += administration_percents[rec.apartment] * rec.amount / 100.0;
+                if (!data[rec.month]['Total']['total']) data[rec.month]['Total']['total'] = {};
+                if (!data[rec.month]['Total']['total']['total']) data[rec.month]['Total']['total']['total'] = 0.00;
+                if (!data[rec.month]['Total']['total'][rec.user]) data[rec.month]['Total']['total'][rec.user] = 0.00;
+                if (!data[rec.month]['Total']['administration']) data[rec.month]['Total']['administration'] = {};
+                if (!data[rec.month]['Total']['administration']['total']) data[rec.month]['Total']['administration']['total'] = 0.00;
+                if (!data[rec.month]['Total']['administration'][rec.user]) data[rec.month]['Total']['administration'][rec.user] = 0.00;
+                data[rec.month]['Total']['total']['total'] += rec.amount
+                data[rec.month]['Total']['total'][rec.user] += rec.amount
+                data[rec.month]['Total']['administration']['total'] += administration_percents[rec.apartment] * rec.amount / 100.0;
+                data[rec.month]['Total']['administration'][rec.user] += administration_percents[rec.apartment] * rec.amount / 100.0;
             })
             console.log(data)
             instance.records.set(data);
@@ -76,39 +87,27 @@ Template.transactions_monthly_report.helpers( {
         return Object.keys(Template.instance().records.get());
     },
     'get_apartment_amount': function (month, apartment) {
+        var usermode = Template.instance().usermode.get();
         var mode = Template.instance().mode.get();
         var data = Template.instance().records.get();
         if (!data[month] || !data[month][apartment]) return "0.00";
-        var res = "0.0";
-        if (mode == 'total') {
-            res = data[month][apartment]['total'];
-        } else if (mode == 'administration') {
-            res = data[month][apartment]['administration'];
-        }
+        var res = data[month][apartment][mode][usermode]
         return res.toFixed(2);
     },
     'get_user_amount': function (month, user) {
-        var mode = Template.instance().mode.get();
-        var data = Template.instance().records.get();
-        if (!data[month] || !data[month][user]) return "0.00";
-        var res = "0.0";
-        if (mode == 'total') {
-            res = data[month][user]['total'];
-        } else if (mode == 'administration') {
-            res = data[month][user]['administration'];
-        }
-        return res.toFixed(2);
-    },
-    'get_total': function (month) {
+        var usermode = Template.instance().usermode.get();
         var mode = Template.instance().mode.get();
         var data = Template.instance().records.get();
         if (!data[month] || !data[month]['Total']) return "0.00";
-        var res = "0.00";
-        if (mode == 'total') {
-            res = data[month]['Total']['total'];
-        } else if (mode == 'administration') {
-            res = data[month]['Total']['administration'];
-        }
+        var res = data[month]['Total'][mode][user];
+        return res.toFixed(2);
+    },
+    'get_total': function (month) {
+        var usermode = Template.instance().usermode.get();
+        var mode = Template.instance().mode.get();
+        var data = Template.instance().records.get();
+        if (!data[month] || !data[month]['Total']) return "0.00";
+        var res = data[month]['Total'][mode][usermode];
         return res.toFixed(2);
     },
     'get_users': function () {
@@ -126,6 +125,10 @@ Template.transactions_monthly_report.helpers( {
     'isCurrentMode': function (mode) {
         return mode == Template.instance().mode.get();
     },
+    'isCurrentUserMode': function (usermode) {
+        return usermode == Template.instance().usermode.get();
+    },
+
 })
 
 Template.transactions_monthly_report.events( {
@@ -142,6 +145,12 @@ Template.transactions_monthly_report.events( {
         template.mode.set(template.$(event.target).parent().attr('mode'));
         Meteor.defer(function () {
            BaseRecord.setupScrollTables(template.$('div.transactions_monthly_report'))
+        });
+    },
+    'click .js-usermode': function (event, template) {
+        template.usermode.set(template.$(event.target).parent().attr('usermode'));
+        Meteor.defer(function () {
+            BaseRecord.setupScrollTables(template.$('div.transactions_monthly_report'))
         });
     },
     'click [data-toggle=collapse]': function (event, template) {

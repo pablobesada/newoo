910a1f1766c7db195a590bd44f8f7218396f65a4
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2016-03-28 21:37:37 -0300

    commit

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+    id: 'com.pdb.becim.departamentos',
+    name: 'Departamentos',
+    description: 'Departamentos',
+    version: '0.0.1'
+});
+
+App.icons({
+    'android_ldpi': 'resources/icons/android-icon-72x72.png',
+    'android_mdpi': 'resources/icons/android-icon-96x96.png',
+    'android_hdpi': 'resources/icons/android-icon-144x144.png',
+    'android_xhdpi': 'resources/icons/android-icon-192x192.png'
+});

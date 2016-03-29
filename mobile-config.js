399bbc8a8575d2499f3c630b69e402d44e2f387f
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2016-03-29 17:08:42 -0300

    commit

--- mobile-config.js
+++ mobile-config.js
@@ -6,7 +6,6 @@ App.info({
 });
 
 App.icons({
-    'android_ldpi': 'resources/icons/android-icon-72x72.png',
     'android_mdpi': 'resources/icons/android-icon-96x96.png',
     'android_hdpi': 'resources/icons/android-icon-144x144.png',
     'android_xhdpi': 'resources/icons/android-icon-192x192.png'
